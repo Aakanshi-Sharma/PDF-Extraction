966048a89108e8b9c4c47d6c4df8fbb5372bfc41
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-01-06 20:45:19 +0530

    app.py updated

---
+++ app.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings , ChatGoogleGenerativeAI
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
+
+
