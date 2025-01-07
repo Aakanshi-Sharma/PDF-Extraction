42d8134e8d17eaca0034cec2a62c7b84276dbc3d
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2025-01-07 20:12:28 +0530

    updates

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings , ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 import google.generativeai as genai
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
@@ -14,3 +14,12 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE-API-KEY"))
 
 
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extractText()
+
+def get_text_chunks():
+    pass
